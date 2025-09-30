4c5dc0084d8b0a75351955a9451a9d98c74b3520
school55minsk-code <school55minsk@gmail.com>
2025-09-30 17:05:44 +0300

    Update update-posts.js

--- api/update-posts.js
+++ api/update-posts.js
@@ -1,18 +1,14 @@
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Метод не разрешён' });
 
   try {
     const posts = req.body; // массив постов
-    await redis.set('posts', posts);
+    await kv.set('posts', posts);
     res.status(200).json({ success: true });
   } catch (err) {
-    res.status(500).json({ error: 'Ошибка записи в Redis' });
+    console.error("KV error:", err);
+    res.status(500).json({ error: 'Ошибка записи в KV' });
   }
 }
