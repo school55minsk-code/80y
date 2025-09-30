323cffc810f3c25e9b17bde95f14ea17f3dcbf6f
school55minsk-code <school55minsk@gmail.com>
2025-09-30 17:05:29 +0300

    Update get-posts.js

--- api/get-posts.js
+++ api/get-posts.js
@@ -1,17 +1,12 @@
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
   try {
-    let posts = await redis.get('posts');
+    let posts = await kv.get('posts');
     if (!posts) posts = []; // если в базе пусто
     res.status(200).json(posts);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Ошибка чтения из Redis' });
+    console.error("KV error:", err);
+    res.status(500).json({ error: 'Ошибка чтения из KV' });
   }
 }
