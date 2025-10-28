8fd9de8b9ee0c45ffb657f9463fd15957032691d
school55minsk-code <school55minsk@gmail.com>
2025-10-28 15:30:30 +0300

    Update get-posts.js

--- api/get-posts.js
+++ api/get-posts.js
@@ -3,10 +3,18 @@ import { kv } from '@vercel/kv';
 export default async function handler(req, res) {
   try {
     let posts = await kv.get('posts');
-    if (!posts) posts = []; // если в базе пусто
+    if (!posts) posts = [];
+
+    // На всякий случай: если в старых постах нет id — добавим
+    posts = posts.map(p => ({
+      ...p,
+      id: p.id || crypto.randomUUID()
+    }));
+
     res.status(200).json(posts);
   } catch (err) {
     console.error("KV error:", err);
     res.status(500).json({ error: 'Ошибка чтения из KV' });
   }
 }
+
